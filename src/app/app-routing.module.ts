185ee128241d401b674b080094d3b39ebc6d8bc7
fsilva <fsilva@stone.com.br>
2017-03-08 18:31:47 -0300

    fix template import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,9 @@ import { DoctorListComponent } from './doctor-list/doctor-list.component';
 import { DoctorDetailComponent } from './doctor-detail/doctor-detail.component';
 
 const routes: Routes = [
-  { path: "", component: DoctorListComponent },
-  { path: "details/:id", component: DoctorDetailComponent },
+  { path: '', component: DoctorListComponent },
+  { path: 'details/:id', component: DoctorDetailComponent },
+  { path: '', redirectTo: '/' }
 ];
 
 @NgModule({
